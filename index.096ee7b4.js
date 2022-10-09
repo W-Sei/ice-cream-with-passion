e3f0912fbbf9ebcfedc6d356e77036e24c3aca66
W-Sei <107546169+W-Sei@users.noreply.github.com>
2022-10-09 11:20:53 +0000

    Deploying to gh-pages from @ W-Sei/ice-cream-with-passion@d86551edd1fc55aa306f83b2447c5a0626a7c4eb 🚀

---
+++ index.096ee7b4.js
@@ -0,0 +1,2 @@
+$(".single-item").slick({autoplay:!0,autoplaySpeed:2e3});
+//# sourceMappingURL=index.096ee7b4.js.map
---
+++ index.9210ff0d.js
@@ -0,0 +1,2 @@
+$(".single-item").slick({autoplay:!0,autoplaySpeed:2e3});
+//# sourceMappingURL=index.9210ff0d.js.map
