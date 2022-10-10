f16e48769869bcbe9591b4b86590144e79a15831
bavariya <109073588+bavariya@users.noreply.github.com>
2022-10-10 13:38:57 +0000

    Deploying to gh-pages from @ W-Sei/ice-cream-with-passion@8896b1e1efffc431b9f019df96e5a1427e2d814f 🚀

---
+++ index.2132bde4.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){window.screen.width>=768?scrollY<=70?document.getElementById("header").classList.remove("header-bgc"):document.getElementById("header").classList.add("header-bgc"):scrollY<=120?document.getElementById("header").classList.remove("header-bgc"):document.getElementById("header").classList.add("header-bgc")};
+//# sourceMappingURL=index.2132bde4.js.map
