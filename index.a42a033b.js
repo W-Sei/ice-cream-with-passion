487a98c1e9694a2561c2d3adba9a9cbf31cddb76
W-Sei <107546169+W-Sei@users.noreply.github.com>
2022-10-12 22:06:35 +0000

    Deploying to gh-pages from @ W-Sei/ice-cream-with-passion@875b43f26de05ce914118a30ca57abf3eaf2b270 🚀

---
+++ index.a42a033b.js
@@ -0,0 +1,2 @@
+const e=document.createElement("div");e.id="pointer-dot";const d=document.createElement("div");d.id="pointer-ring",document.body.insertBefore(e,document.body.children[0]),document.body.insertBefore(d,document.body.children[0]);
+//# sourceMappingURL=index.a42a033b.js.map
