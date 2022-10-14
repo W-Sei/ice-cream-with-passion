07fa83308bf86a8c3f21b2fda675bc34ab8af5bd
Hanna Franko <sei.wrcl@gmail.com>
2022-10-14 12:53:53 +0300

    modals-working

---
+++ src/scripts-js/locations-modal.js
@@ -0,0 +1,21 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-locations');
+  const openMenuBtn = document.querySelector('.js-locations-open');
+  const closeMenuBtn = document.querySelector('.js-locations-close');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  
+  })();
