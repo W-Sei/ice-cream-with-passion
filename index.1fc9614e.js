0e00e47bf74e95172920ce8abaeda2b60bc33721
W-Sei <107546169+W-Sei@users.noreply.github.com>
2022-10-14 11:07:02 +0000

    Deploying to gh-pages from @ W-Sei/ice-cream-with-passion@a39a3c39a65231d634adf3a040fadce215c36081 🚀

---
+++ index.1fc9614e.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-menu]"),openModalBtn:document.querySelector("[data-modal-menu-open]"),closeModalBtn:document.querySelector("[data-modal-menu-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.1fc9614e.js.map
---
+++ index.6db93d3c.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal]"),openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.6db93d3c.js.map
