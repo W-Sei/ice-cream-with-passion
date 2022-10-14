59b2113a282e1df4d84bb7c84a4411f5967edd38
Hanna Franko <sei.wrcl@gmail.com>
2022-10-14 10:35:09 +0300

    change scripts

--- src/scripts-js/mobile-menu.js
+++ src/scripts-js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuBtn22 = document.querySelector('.js-menu-close22');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -15,8 +16,25 @@
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
+
+  $('.mobile-navigation-link').on("click", function() {
+    //this == the link that was clicked
+    // var href = $(this).attr("href");
+    // alert("You're trying to go to " + href);
+    	  mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+});
+  
+
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  
+  closeMenuBtn22.addEventListener('click', () => {
+	  mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
